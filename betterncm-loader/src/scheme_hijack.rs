436598416ef5489ab5e96e531f8563a33738d5f8
SteveXMH <39523898+Steve-xmh@users.noreply.github.com>
2023-02-17 00:06:02 +0800

    加点域

--- betterncm-loader/src/scheme_hijack.rs
+++ betterncm-loader/src/scheme_hijack.rs
@@ -13,13 +13,13 @@ pub fn on_process_url_data(url: &str, data: Vec<u8>) -> Vec<u8> {
             let document = nipper::Document::from(&data);
 
             document.select("meta[http-equiv=\"Content-Security-Policy\"]").set_attr("content", 
-            "default-src * data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval';\
-            script-src * data: blob: 'unsafe-inline' 'unsafe-eval';\
-            connect-src * data: blob: 'unsafe-inline';\
-            img-src * data: blob: 'unsafe-inline';\
-            frame-src * data: blob:;\
-            style-src * data: blob: 'unsafe-inline';\
-            font-src * data: blob: 'unsafe-inline';\
+            "default-src * mrbncm: data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval';\
+            script-src * mrbncm: data: blob: 'unsafe-inline' 'unsafe-eval';\
+            connect-src * mrbncm: data: blob: 'unsafe-inline';\
+            img-src * mrbncm: data: blob: 'unsafe-inline';\
+            frame-src * mrbncm: data: blob:;\
+            style-src * mrbncm: data: blob: 'unsafe-inline';\
+            font-src * mrbncm: data: blob: 'unsafe-inline';\
             frame-ancestors * data: blob: 'unsafe-inline';");
 
             return document.html().to_string().into_bytes();
