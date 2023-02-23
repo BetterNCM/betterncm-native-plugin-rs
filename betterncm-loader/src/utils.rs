b5f0fc7b6dc7898bd45f357a67c40e6a6671c1ad
SteveXMH <39523898+Steve-xmh@users.noreply.github.com>
2023-02-23 17:38:26 +0800

    做好了hijack了

--- betterncm-loader/src/utils.rs
+++ betterncm-loader/src/utils.rs
@@ -3,6 +3,55 @@ use std::{
     path::Path,
 };
 
+use windows::Win32::Storage::FileSystem::{
+    GetFileVersionInfoSizeW, GetFileVersionInfoW, VerQueryValueW, VS_FIXEDFILEINFO,
+};
+
+pub fn get_ncm_version() -> semver::Version {
+    unsafe {
+        let exe_file = std::env::current_exe()
+            .unwrap()
+            .to_string_lossy()
+            .to_string();
+        let exe_file = windows::core::HSTRING::from(exe_file);
+        let mut ver_handle = 0;
+        let ver_size = GetFileVersionInfoSizeW(&exe_file, Some(&mut ver_handle));
+
+        if ver_size != 0 {
+            let mut data = vec![0u8; ver_size as usize];
+            if GetFileVersionInfoW(&exe_file, ver_handle, ver_size, data.as_mut_ptr() as _)
+                .as_bool()
+            {
+                let mut size = 0;
+                let mut info: *mut VS_FIXEDFILEINFO = std::ptr::null_mut();
+                if VerQueryValueW(
+                    data.as_mut_ptr() as _,
+                    windows::w!("\\"),
+                    &mut info as *mut _ as _,
+                    &mut size,
+                )
+                .as_bool()
+                    && size != 0
+                {
+                    if let Some(info) = info.as_ref() {
+                        if info.dwSignature == 0xfeef04bd {
+                            let major_ver = (info.dwFileDateMS >> 16) & 0xFFFF;
+                            let minor_ver = info.dwFileDateMS & 0xFFFF;
+                            let build_ver = (info.dwFileDateLS >> 16) & 0xFFFF;
+                            return semver::Version::new(
+                                major_ver as _,
+                                minor_ver as _,
+                                build_ver as _,
+                            );
+                        }
+                    }
+                }
+            }
+        }
+        semver::Version::new(0, 0, 0)
+    }
+}
+
 pub fn unzip_inner(f: impl Read + Seek, unzip_dir: impl AsRef<Path>) {
     if let Ok(mut zipfile) = zip::ZipArchive::new(f) {
         for i in 0..zipfile.len() {
