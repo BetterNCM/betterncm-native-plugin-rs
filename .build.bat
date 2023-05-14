9ff5dbf273fea7f5497b83ff2f58d73bccd49ba9
SteveXMH <39523898+Steve-xmh@users.noreply.github.com>
2023-05-14 14:21:20 +0800

    准备推到上游

--- .build.bat
+++
@@ -1,5 +0,0 @@
-cargo build
-copy target\i686-pc-windows-msvc\debug\betterncm_plugin_example.dll C:\betterncm\plugins_dev\betterncm-plugin-rs\
-copy target\i686-pc-windows-msvc\debug\betterncm_plugin_example.dll.exp C:\betterncm\plugins_dev\betterncm-plugin-rs\
-copy target\i686-pc-windows-msvc\debug\betterncm_plugin_example.pdb C:\betterncm\plugins_dev\betterncm-plugin-rs\
-copy target\i686-pc-windows-msvc\debug\betterncm_plugin_example.dll.lib C:\betterncm\plugins_dev\betterncm-plugin-rs\
--- betterncm-loader/src/api/internal.rs
+++
@@ -1,8 +0,0 @@
-use betterncm_macro::*;
-use tracing::*;
-
-#[betterncm_native_api(name = "internal.getFrameworkCSS")]
-#[instrument]
-pub fn get_framework_css() -> anyhow::Result<String> {
-    Ok(include_str!("../../resources/framework.css").into())
-}
